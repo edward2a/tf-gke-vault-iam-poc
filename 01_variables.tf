6ef9e3803bc568c681ae3bd192f62f5e6d52a33b
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-02-05 18:47:39 +0000

    add some basic config

---
+++ 01_variables.tf
@@ -0,0 +1,4 @@
+variable google_credentials {}
+variable google_default_project {}
+variable google_default_region { default = "europe-west2" }
+variable google_default_zone { default = "europe-west2-b" }
