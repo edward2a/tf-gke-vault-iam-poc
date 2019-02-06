b0d4f4cf2546fe698fdbd88734f1491e74157f90
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-02-06 19:51:28 +0000

    disable gitlab runners; add region

--- 50_main.tf
+++ 50_main.tf
@@ -20,8 +20,8 @@ module vault {
   project_id        = "${var.google_default_project}"
   region            = "${var.google_default_region}"
   zone              = "${var.google_default_zone}"
-  network           = "${google_compute_network.kubault_poc.self_link}"
-  subnetwork        = "${google_compute_subnetwork.kubault_poc_1.self_link}"
+  network           = "${google_compute_network.kubault_poc.name}"
+  subnetwork        = "${google_compute_subnetwork.kubault_poc_1.name}"
   kms_keyring_name  = "${google_kms_key_ring.vault_poc.name}"
 
   storage_bucket    = "vault-poc"
@@ -54,13 +54,16 @@ module gitlab {
   project     = "${var.google_default_project}"
   prefix      = "gl-poc"
 
+  region      = "${var.google_default_region}"
+  zone        = "${var.google_default_zone}"
   network     = "${google_compute_network.kubault_poc.self_link}"
   subnetwork  = "${google_compute_subnetwork.kubault_poc_1.self_link}"
-  zone        = "${var.google_default_zone}"
 
   ssh_key     = "./ssh_key/id_rsa"
   data_volume = "gitlab-poc-data"
   config_file = ""
   dns_name    = "gitlab"
   dns_zone    = "no_dns"
+
+  runner_count = 0
 }
