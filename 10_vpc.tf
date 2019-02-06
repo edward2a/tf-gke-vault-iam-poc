704721e11d5c87be162a31a51247f4b087da9f90
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-02-06 09:45:38 +0000

    fix subnet secondary range name

--- 10_vpc.tf
+++ 10_vpc.tf
@@ -10,7 +10,7 @@ resource google_compute_subnetwork kubault_poc_1 {
   network = "${google_compute_network.kubault_poc.self_link}"
   ip_cidr_range = "172.16.200.0/24"
   secondary_ip_range = [{
-    range_name = "kubault_poc_1_k8s"
+    range_name = "kubault-poc-1-k8s"
     ip_cidr_range = "172.16.201.0/24"
   }]
   private_ip_google_access = "true"
