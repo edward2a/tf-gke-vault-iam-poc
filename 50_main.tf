6453e5da0098ddc286cf01ff509de243b9738fb8
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-02-07 17:00:53 +0000

    Add suffix for kms stuff to deal with long deletion times Fix ip range referencing in gke module Add persistent disk for gitlab

--- 50_main.tf
+++ 50_main.tf
@@ -1,15 +1,19 @@
 
 #### VAULT ####
 
+resource random_id vault_key_name_suffix {
+  byte_length = 8
+}
+
 # vault keyring
 resource google_kms_key_ring vault_poc {
-  name     = "vault-poc"
+  name     = "vault-poc-${random_id.vault_key_name_suffix.hex}"
   location = "global"
 }
 
 # vault key
 resource google_kms_crypto_key vault_poc {
-  name            = "vault-poc"
+  name            = "vault-poc-${random_id.vault_key_name_suffix.hex}"
   key_ring        = "${google_kms_key_ring.vault_poc.self_link}"
   rotation_period = "100000s"
 }
@@ -24,7 +28,7 @@ module vault {
   subnetwork        = "${google_compute_subnetwork.kubault_poc_1.name}"
   kms_keyring_name  = "${google_kms_key_ring.vault_poc.name}"
 
-  storage_bucket    = "vault-poc"
+  storage_bucket    = "vault-poc-${random_id.vault_key_name_suffix.hex}"
   force_destroy_bucket = "true"
 
   vault_version     = "1.0.2"
@@ -42,11 +46,18 @@ module gke {
 
   network             = "${google_compute_network.kubault_poc.name}"
   subnetwork          = "${google_compute_subnetwork.kubault_poc_1.name}"
-  ip_range_pods       = "${google_compute_subnetwork.kubault_poc_1.secondary_ip_range.range_name}"
-  ip_range_services   = "${google_compute_subnetwork.kubault_poc_1.secondary_ip_range.range_name}"
+  ip_range_pods       = "${google_compute_subnetwork.kubault_poc_1.secondary_ip_range.0.range_name}"
+  ip_range_services   = "${google_compute_subnetwork.kubault_poc_1.secondary_ip_range.0.range_name}"
 }
 
 #### GITLAB ####
+
+resource google_compute_disk gitlab_data {
+  name = "gitlab-poc-data"
+  type = "pd-standard"
+  size = "20"
+  zone = "${var.google_default_zone}"
+}
 
 module gitlab {
   source = "./modules/gcp/gitlab"
@@ -60,7 +71,7 @@ module gitlab {
   subnetwork  = "${google_compute_subnetwork.kubault_poc_1.self_link}"
 
   ssh_key     = "./ssh_key/id_rsa"
-  data_volume = "gitlab-poc-data"
+  data_volume = "${google_compute_disk.gitlab_data.name}"
   config_file = ""
   dns_name    = "gitlab"
   dns_zone    = "no_dns"
