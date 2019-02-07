192180e5ed288ed2706f7512c7c1dd99016688a9
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-02-07 17:00:11 +0000

    fix network reference; update ranges to fit gke

--- 10_vpc.tf
+++ 10_vpc.tf
@@ -7,13 +7,11 @@ resource google_compute_network kubault_poc {
 resource google_compute_subnetwork kubault_poc_1 {
   name = "kubault-poc-1"
   region = "${var.google_default_region}"
-  network = "${google_compute_network.kubault_poc.self_link}"
-  ip_cidr_range = "172.16.200.0/24"
+  network = "${google_compute_network.kubault_poc.name}"
+  ip_cidr_range = "172.16.200.0/22"
   secondary_ip_range = [{
     range_name = "kubault-poc-1-k8s"
-    ip_cidr_range = "172.16.201.0/24"
+    ip_cidr_range = "172.16.208.0/21"
   }]
   private_ip_google_access = "true"
 }
-
-
